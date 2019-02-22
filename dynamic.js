7b2be274ca57d5d7383bc0b3a7559fd2592fe290
vindhyarao35 <47808925+vindhyarao35@users.noreply.github.com>
2019-02-22 15:29:48 +0530

    Add files via upload

--- dynamic.js
+++ dynamic.js
@@ -1,89 +1,89 @@
-function getdata(file,callback){
-    var xhr=new XMLHttpRequest();
-   xhr.overrideMimeType("application/json");
-    xhr.open("GET",file,true);
-    xhr.onreadystatechange=function(){
-    	if(xhr.readyState===4 && xhr.status=="200"){
-    		callback(xhr.responseText);
-    }    
-    	}
+function getdata(file,callback) {
+var xhr= new XMLHttpRequest();
+xhr.overrideMimeType("application/json");
+xhr.open("GET",file,true);
+xhr.onreadystatechange=function(){
+	if(xhr.readyState===4 && xhr.status=="200"){
+		callback(xhr.responseText)
+	}
+}
 xhr.send(null);
 }
 getdata("dynamic.json",function(text){
-	var data=JSON.parse(text);
-	console.log(data);
- personalinfo(data.profile);
- carrier(data.carrier);
- educational(data.educational);
+      var data=JSON.parse(text);
+      console.log(data);
+       personalinfo(data.profile);
+       career(data.career);
+       educational(data.educational);
 })
-//  // leftdiv
- var left=document.querySelector(".leftdiv");
+ // leftdiv
+var left=document.querySelector(".leftdiv");
  function personalinfo(pi){
- 
-     var image=document.createElement("img");
-   // image.src="1234.jpg";
-    image.src=pi.img;
-   left.appendChild(image);
 
-    var name=document.createElement("h2");
-    name.textContent=pi.name;
-    left.appendChild(name);
 
-    var roll=document.createElement("h4");
-    roll.textContent=pi.roll;
-    left.appendChild(roll);
+var image=document.createElement("img");
+      // image.src="1235.png";
+  image.src=pi.img;
+     left.appendChild(image);
 
-    var hr=document.createElement("hr")
-    left.appendChild(hr);
+     var name=document.createElement("h2");
+     name.textContent=pi.name;
+     left.appendChild(name);
 
-  var place=document.createElement("p");
-   place.textContent=pi.place;
-   left.appendChild(place);
+     var roll=document.createElement("h4");
+     roll.textContent=pi.roll;
+     left.appendChild(roll);
 
-    var email=document.createElement("p");
-    email.textContent=pi.email;
-    left.appendChild(email); 
-}
- // right div
+     var hr=document.createElement("hr");
+     left.appendChild(hr);
+
+     var place=document.createElement("p");
+     place.textContent=pi.place;
+     left.appendChild(place);
+
+     var gmail=document.createElement("p");
+     gmail.textContent=pi.gmail;
+     left.appendChild(gmail);
+ }
+ // rightdiv
  var right=document.querySelector(".rightdiv");
-   // carrier objective
-   function carrier(ca){
+ // career objective
+ function career(ca){
 
-   	  var heading=document.createElement("h1");
-   	      heading.textContent="Carrier Objective";
-          right.appendChild(heading);
+ 	var heading=document.createElement("h1");
+ 	    heading.textContent="Career Objective";
+        right.appendChild(heading);
 
-      var hr=document.createElement("hr");
-      right.appendChild(hr);
-      
-      var carrier=document.createElement("p");
-      carrier.textContent=ca.info;
-      right.appendChild(carrier);
-   }
-   // education details
-    function educational(ed){
+        var hr=document.createElement("hr");
+        right.appendChild(hr);
 
-    	var heading=document.createElement("h1");
-    	  heading.textContent="Education Details";
-    	  right.appendChild(heading);
+        var career=document.createElement("p");
+        career.textContent=ca.info;
+        right.appendChild(career);
+}
+        //educational details
+        function educational(ed){
+        	var heading=document.createElement("h1");
+        	    heading.textContent="Education Details";
+        	    right.appendChild(heading);
 
-    	   var hr=document.createElement("hr");
-    	   right.appendChild(hr);
-               
-            var table=document.createElement("table");
-             table.border="2";
-             var tr1="<tr><th>degree</th><th>institute</th><th>per</th><th>yop</th></tr>";
-             var tr2="";
-             for(var i=0;i<ed.length;i++) {
-             	// tr2=tr2+"<tr><td>"+ed[i].degree+"</td><td>"+ed[i].institute+"</td><td>"+ed[i].per+"</td><td>"+ed[i].yop+"</tr>"
-             	tr2=tr2+"<tr><td>"+ed[i].degree+"</td><td>"+ed[i].institute+"</td><td>"+ed[i].per+"</td><td>"+ed[i].yop+"</td><tr>";           
-             	 }
-               table.innerHTML=tr1+tr2;
-               right.appendChild(table);
+        	     var hr=document.createElement("hr");
+        	     right.appendChild(hr);
 
-           }
+        	     var table=document.createElement("table");
+        	         table.border="2";
+        	         var tr1="<tr><th>degree</th><th>institute</th><th>per</th><th>yop</th></tr>"
+                     var tr2="";
+                     for(var i=0;i<ed.length;i++){
+                     	// tr2=tr2+"<tr><td>"+ed[i].degree+"<td><td>"+ed[i].institute+"</td><td>"+ed[i].per"</td><td>"+ed[i].yop+"</tr>"
+                     	tr2=tr2+"<tr><td>"+ed[i].degree+"</td><td>"+ed[i].institute+"</td><td>"+ed[i].per+"</td><td>"+ed[i].yop+"</td><tr>";
+}
+                         table.innerHTML=tr1+tr2;
+                         right.appendChild(table);                    
 
 
+        } 
+ 
 
-
+ 
 
